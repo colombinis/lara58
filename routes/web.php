039942da5775b03ebe91d109726702511cf5d4e7
Sebastian A Colombini <colombinis@gmail.com>
2019-10-10 19:09:29 -0300

    Agrega auth

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,7 @@ Route::get('/', function () {
 Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
